8a6b75b6888347b1874922850c70e5e3033f9e3e
haxr369 <haxr369@gmail.com>
2023-02-25 17:24:36 +0900

    프로젝트 자동배포 완성!!!!!!!!!1

--- APIs/requirements.sh
+++
@@ -1,6 +0,0 @@
-pip install PyJWT
-pip install timm==0.5.4
-pip install opencv-python
-apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
-apt-get install libgl1-mesa-glx
-apt-get install libglib2.0-0
